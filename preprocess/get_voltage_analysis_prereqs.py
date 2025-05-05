7bcdd6c477f7c95fdffd95e9bf1b1ee2159a4629
Saul G. Lopez <saulglopez@Sauls-MacBook-Pro.local>
2025-05-05 11:22:21 -0500

    minor nonfunctional changes

--- preprocess/get_voltage_analysis_prereqs.py
+++ preprocess/get_voltage_analysis_prereqs.py
@@ -4,15 +4,15 @@ from pathlib import Path
 import pandas as pd
 
 def get_data_df(df_dir) -> dict:
-    '''
+    """
         Load voltage data from csv files into pandas dataframes by reading from dataframe.
-        
+
         Parameters:
             df_dir: directory to the location of dataframe
 
         Returns:
             test_info: dictionary containing dataframes with voltage data.
-    '''
+    """
     raw_data_dir = '/data/project/nvl_lab/HoloBMI/Raw/'
     df_path = Path(df_dir)
     df_data = pd.read_parquet(df_path)
@@ -128,7 +128,7 @@ def get_data_rec(raw_data_dir) -> dict:
                 if 0 <= mouse_idx < len(data_paths[date_input]):
                     datasets = [data_paths[date_input][mouse_idx]]
                 else:
-                    print('Invlid input')
+                    print('Invalid input')
                     exit(1)
             else:
                 datasets = data_paths[date_input]           
@@ -146,7 +146,7 @@ def get_data_rec(raw_data_dir) -> dict:
         #flag_names3 = ['191015_NVI12_D20', '191103_NVI13_D34', '191103_NVI16_D34', '191105_NVI17_D01', '191106_NVI22_D02','191111_NVI20_D07', '191115_NVI20_D11', '191212_NVI22_D27', '191212_NVI20_D27', '191212_NVI17_D27', '191213_NVI20_D28', '191214_NVI17_D29']
         ds_name = f'{ds_path.parts[-3]}/{ds_path.parts[-2]}/{ds_path.parts[-1]}'
         flag_names3 = ['191106/NVI20/D02','191015/NVI12/D20', '191103/NVI13/D34', '191103/NVI16/D34', '191105/NVI17/D01', '191106/NVI22/D02','191111/NVI20/D07', '191115/NVI20/D11', '191212/NVI22/D27', '191212/NVI20/D27', '191212/NVI17/D27', '191213/NVI20/D28', '191214/NVI17/D29']
-        # No ' Input 0', only one mat, no pre-train data, no baseline data, no mat files, extra baseline, no pretrain/bmi mat, baseline volt is not csv, no pretrain/bmi mat, ", ", no bmi no rewrd csv, no pretrain/bmi mat
+        # No ' Input 0', only one mat, no pre-train data, no baseline data, no mat files, extra baseline, no pretrain/bmi mat, baseline volt is not csv, no pretrain/bmi mat, ", ", no bmi no reward csv, no pretrain/bmi mat
         if ds_name not in good_names:
             continue
         #if ds_name in flag_names:
@@ -163,7 +163,7 @@ def get_data_rec(raw_data_dir) -> dict:
         bmi_mat = online_data[1]
         base_mat = online_data[2]
         
-        # Voltage viles
+        # Voltage files
         test_names = [d.name for d in ds_path.glob("im/*") if not d.name.startswith('.')]
         # tiff counts should be the same for each dataset
         #test_info[f'{ds_path.parts[-3]}_{ds_path.parts[-2]}_{ds_path.parts[-1]}'] = {'mats': [holo_mat, base_mat, pre_mat, bmi_mat]}
