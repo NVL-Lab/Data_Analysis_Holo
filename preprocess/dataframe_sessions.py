bd5da764a8038134bb4efb0e046a0ea5e0109f96
AndreaDieffe <adifranc@uab.edu>
2025-09-22 11:53:25 -0500

    uptdate to work on cluster

--- preprocess/dataframe_sessions.py
+++ preprocess/dataframe_sessions.py
@@ -1,6 +1,9 @@
 
 import collections
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pandas as pd
 import numpy as np
 from pathlib import Path
