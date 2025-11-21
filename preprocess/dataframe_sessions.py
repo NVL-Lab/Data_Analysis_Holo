78adefac238904d1aaa03e0a8651e118fa8b29d8
AndreaDieffe <adifranc@uab.edu>
2025-11-21 11:27:14 -0600

    Lowercase columns
    Hand changed columns to lowercase because Nuria thinks it's more professional

--- preprocess/dataframe_sessions.py
+++ preprocess/dataframe_sessions.py
@@ -9,7 +9,7 @@ import numpy as np
 from pathlib import Path
 from datetime import datetime
 import re
-from preprocess.session_paths import _hE2_rew,_hE2_norew,_hE2_rew_fb,_hE3_rew,_No_Reward_Pretrain,_randrew,_randrew_fb,_BMI
+from session_paths import _hE2_rew,_hE2_norew,_hE2_rew_fb,_hE3_rew,_No_Reward_Pretrain,_randrew,_randrew_fb,_BMI
 from utils.analysis_constants import AnalysisConstants as act
 
 # def get_all_sessions() -> pd.DataFrame:
@@ -39,20 +39,9 @@ from utils.analysis_constants import AnalysisConstants as act
 
 
 
-def get_all_sessions(folder_save: Path) -> pd.DataFrame:
-    """ function to put all the experiment types in a dataframe"""
-    df = pd.DataFrame()
-    experiment_types = {'hE2_rew', 'hE2_norew', 'hE3_rew', 'randrew'}
-    for experiment in experiment_types:
-        df_new = get_sessions_df(experiment)
-        df = pd.concat([df, df_new], ignore_index=True)
-    df.to_csv(folder_save / 'df_holobmi.csv', index=False)
-    return df
 
 
 def get_sessions_df(experiment_type: str) -> pd.DataFrame:
-    """ Function to put all the experiments from an experiment type in a dataframe
-    """
     experiment_dict = {
         'hE2_rew': _hE2_rew,
         'hE2_norew': _hE2_norew,
@@ -94,20 +83,20 @@ def get_sessions_df(experiment_type: str) -> pd.DataFrame:
             }
             # Initialize data columns with None, Flags with False
             for category in category_map.values():
-                if f'Flag_session_more_tif_{category}' not in ret:
-                    ret[f'Flag_session_more_tif_{category}'] = []
+                if f'flag_session_more_tif_{category}' not in ret:
+                    ret[f'flag_session_more_tif_{category}'] = []
                 ret[category].append(None)
                 ret[f'{category}_voltage_file'].append(None)  # Updated voltage naming
-                ret[f'Flag_{category}'].append(False)  # Flagging for multiple files
-                ret[f'Flag_session_more_tif_{category}'].append(False)
-                ret[f'Flag_{category}_voltage_file'].append(False)
+                ret[f'flag_{category}'].append(False)  # Flagging for multiple files
+                ret[f'flag_session_more_tif_{category}'].append(False)
+                ret[f'flag_{category}_voltage_file'].append(False)
 
 
-            ret['Pretrain_im'].append(None)
-            ret['Flag_Pretrain_im'].append(False)
-            ret['Pretrain_im_voltage_file'].append(None)
-            ret['Flag_session_more_tif_pretrain'].append(False)
-            ret[f'Flag_Pretrain_im_voltage_file'].append(False)
+            ret['pretrain_im'].append(None)
+            ret['flag_pretrain_im'].append(False)
+            ret['pretrain_im_voltage_file'].append(None)
+            ret['flag_session_more_tif_pretrain'].append(False)
+            ret[f'flag_pretrain_im_voltage_file'].append(False)
             # Iterate over session directory
             for file_name in os.listdir(dir_files):
                 file_path = dir_files / file_name
@@ -120,31 +109,31 @@ def get_sessions_df(experiment_type: str) -> pd.DataFrame:
                         for key, category in category_map.items():
                             
                             if not any(key in subfolder for subfolder in im_subfolders):
-                                    ret[f'Flag_{category}'][-1] = True
+                                    ret[f'flag_{category}'][-1] = True
                             if im_subfolder.startswith(key):
                                 if not os.listdir(im_subfolder_path):
-                                    ret[f'Flag_{category}'][-1] = True
-                                    ret[f'Flag_session_more_tif_{category}'][-1] = True
+                                    ret[f'flag_{category}'][-1] = True
+                                    ret[f'flag_session_more_tif_{category}'][-1] = True
                                     continue 
                                 im_files = [subfolder for subfolder in os.listdir(im_subfolder_path) if subfolder.startswith(f"{key}_")]
                                 parsed_folder_name = f"{key}/{im_files[0]}" if im_files else None
                                 # Check if multiple files exist
                                 if len(im_files) > 1 or not im_files:
-                                    ret[f'Flag_{category}'][-1] = True  # Flag session
+                                    ret[f'flag_{category}'][-1] = True  # Flag session
                                     ret[category][-1] = None  # No single file assigned
-                                    ret[f'Flag_session_more_tif_{category}'][-1] = False
+                                    ret[f'flag_session_more_tif_{category}'][-1] = False
 
                                 elif len(im_files) == 1:
                                     ret[category][-1] = parsed_folder_name  
-                                    ret[f'Flag_{category}'][-1] = False  
+                                    ret[f'flag_{category}'][-1] = False  
                                     parsed_folder = im_subfolder_path / im_files[0]
                                     num_tiff_files = sum(1 for file in os.listdir(parsed_folder) if file.endswith(".tif"))
 
-                                    ret[f'Flag_session_more_tif_{category}'][-1] = True if num_tiff_files > tiff_limits[key] else False
+                                    ret[f'flag_session_more_tif_{category}'][-1] = True if num_tiff_files > tiff_limits[key] else False
                                 else:
                                     ret[category][-1] = None
-                                    ret[f'Flag_{category}'][-1] = False  # No flag needed
-                                    ret[f'Flag_session_more_tif_{category}'][-1] = False 
+                                    ret[f'flag_{category}'][-1] = False  # No flag needed
+                                    ret[f'flag_session_more_tif_{category}'][-1] = False 
 
                                 # Look for voltage recording file inside the correct subfolder
                                 if im_files:
@@ -154,43 +143,43 @@ def get_sessions_df(experiment_type: str) -> pd.DataFrame:
 
                                     if voltage_file_path.exists():
                                         ret[f'{category}_voltage_file'][-1] = voltage_file_pattern
-                                        ret[f'Flag_{category}_voltage_file'][-1] = False
+                                        ret[f'flag_{category}_voltage_file'][-1] = False
                                     else:
                                         ret[f'{category}_voltage_file'][-1] = None
-                                        ret[f'Flag_{category}_voltage_file'][-1] = True
+                                        ret[f'flag_{category}_voltage_file'][-1] = True
                         
 
                         # Checking if "_pretrain" is missing in any subfolder names
                         if not any("_pretrain" in subfolder for subfolder in im_subfolders):
-                            ret['Flag_Pretrain_im'][-1] = True                    
+                            ret['flag_pretrain_im'][-1] = True                    
 
                         if "_pretrain" in im_subfolder:
                                 if not os.listdir(im_subfolder_path):
-                                    ret['Flag_Pretrain_im'][-1] = True
-                                    ret['Flag_session_more_tif_pretrain'][-1] = True
+                                    ret['flag_pretrain_im'][-1] = True
+                                    ret['flag_session_more_tif_pretrain'][-1] = True
                                     continue 
                                 im_files = [subfolder for subfolder in os.listdir(im_subfolder_path) if "_pretrain" in subfolder]
                                 parsed_folder_name = f"{im_subfolder}/{im_files[0]}" if im_files else None
 
                                 if len(im_files) > 1 or not im_files:
-                                    ret['Flag_Pretrain_im'][-1] = True
-                                    ret['Pretrain_im'][-1] = None
-                                    ret['Flag_session_more_tif_pretrain'][-1] = False
+                                    ret['flag_pretrain_im'][-1] = True
+                                    ret['pretrain_im'][-1] = None
+                                    ret['flag_session_more_tif_pretrain'][-1] = False
 
                                 elif len(im_files) == 1:
-                                    ret['Pretrain_im'][-1] = parsed_folder_name
-                                    ret['Flag_Pretrain_im'][-1] = False
+                                    ret['pretrain_im'][-1] = parsed_folder_name
+                                    ret['flag_pretrain_im'][-1] = False
                                     parsed_folder = im_subfolder_path / im_files[0]
                                     num_tiff_files = sum(1 for file in os.listdir(parsed_folder) if file.endswith(".tif"))
                                     if num_tiff_files > tiff_limits['pretrain']:
-                                        ret['Flag_session_more_tif_pretrain'][-1] = True
+                                        ret['flag_session_more_tif_pretrain'][-1] = True
                                     else:
-                                        ret['Flag_session_more_tif_pretrain'][-1] = False
+                                        ret['flag_session_more_tif_pretrain'][-1] = False
 
                                 else:
-                                    ret['Pretrain_im'][-1] = None
-                                    ret['Flag_Pretrain_im'][-1] = False   
-                                    ret['Flag_session_more_tif_pretrain'][-1] = False
+                                    ret['pretrain_im'][-1] = None
+                                    ret['flag_pretrain_im'][-1] = False   
+                                    ret['flag_session_more_tif_pretrain'][-1] = False
 
                                 if im_files:
                                     full_subfolder_name = im_files[0]
@@ -198,11 +187,11 @@ def get_sessions_df(experiment_type: str) -> pd.DataFrame:
                                     pretrain_voltage_file_path = im_subfolder_path / full_subfolder_name / pretrain_voltage_file_pattern
                                     
                                     if pretrain_voltage_file_path.exists():
-                                        ret['Pretrain_im_voltage_file'][-1] = pretrain_voltage_file_pattern
-                                        ret[f'Flag_Pretrain_im_voltage_file'][-1] = False
+                                        ret['pretrain_im_voltage_file'][-1] = pretrain_voltage_file_pattern
+                                        ret[f'flag_pretrain_im_voltage_file'][-1] = False
                                     else:
-                                        ret['Pretrain_im_voltage_file'][-1] = None
-                                        ret[f'Flag_Pretrain_im_voltage_file'][-1] = True
+                                        ret['pretrain_im_voltage_file'][-1] = None
+                                        ret[f'flag_pretrain_im_voltage_file'][-1] = True
 
 
             # Additional file processing
@@ -218,31 +207,31 @@ def get_sessions_df(experiment_type: str) -> pd.DataFrame:
                 if file_name.startswith('roi'):
                     roi_mat_files.append(file_name)
                 elif file_name[:5] == 'BOT_c':
-                    ret['Bot_candidates'].append(str(file_name))
+                    ret['bot_candidates'].append(str(file_name))
                 elif file_name[:5] == 'BOT_e':
-                    ret['Bot_ensemble'].append(str(file_name))
+                    ret['bot_ensemble'].append(str(file_name))
                 elif file_name[:5] == 'GPL_c':
-                    ret['GPL_candidates'].append(str(file_name))
+                    ret['gpl_candidates'].append(str(file_name))
                 elif file_name[:5] == 'GPL_e':
-                    ret['GPL_ensemble'].append(str(file_name))
+                    ret['gpl_ensemble'].append(str(file_name))
                 elif file_name[:5] == 'XML_c':
-                    ret['XML_candidates'].append(str(file_name))
+                    ret['xml_candidates'].append(str(file_name))
                 elif file_name[:5] == 'XML_e':
-                    ret['XML_ensemble'].append(str(file_name))
+                    ret['xml_ensemble'].append(str(file_name))
                 elif file_name.startswith('holostim_seq') and file_name.endswith('.mat'):
                     # ret['Holostim_seq_mat_file'].append(file_name)
                     holostim_seq_files.append(str(file_name))
                 elif file_name.startswith('workspace'):
-                    ret['Workspace_mat_file'].append(str(file_name))
+                    ret['workspace_mat_file'].append(str(file_name))
                 elif file_name.startswith('holoMask'):
-                    ret['HoloMask_gpl_file'].append(str(file_name))
+                    ret['holoMask_gpl_file'].append(str(file_name))
                 elif file_name.startswith('mainProt'):
                     # ret['MainProt_file'].append(str(file_name))
                     mainProt_files.append(str(file_name))
                 elif file_name.startswith('seq_single'):
-                    ret['XML_holostim_seq'].append(str(file_name))
+                    ret['xml_holostim_seq'].append(str(file_name))
                 elif file_name.startswith('strcMask'):
-                    ret['StrcMask_mat_file'].append(str(file_name))
+                    ret['strc_mask_mat_file'].append(str(file_name))
                 elif file_name.startswith('BMI_target_info'):
                     bmitarget_files.append(str(file_name))
                 elif file_name.startswith('BaselineOnline') and file_name.endswith('.mat'):
@@ -254,41 +243,40 @@ def get_sessions_df(experiment_type: str) -> pd.DataFrame:
                     if match:
                         bmi_files.append((file_name, datetime.strptime(match.group(1), "%y%m%dT%H%M%S")))
 
-            ret['Roi_mat_file'].extend(roi_mat_files)
-            ret['BMI_target_mat_file'].append(bmitarget_files[0] if len(bmitarget_files) == 1 else None)
-            ret['Baseline_mat_file'].append(baseline_files[0] if len(baseline_files) == 1 else None)
-            ret['Flag_BaselineOnline'].append(True if len(baseline_files) > 1 else False)
-            ret['Flag_BMITarget'].append(True if len(bmitarget_files) > 1 else False)
-            ret['Target_calibration_mat_file'].append(target_files[0] if len(target_files) == 1 else None)
-            ret['Flag_target_calibration'].append(True if len(target_files) > 1 else False)
-            ret['Holostim_seq_mat_file'].append(holostim_seq_files[0] if len(holostim_seq_files) == 1 else None)
+            ret['roi_mat_file'].extend(roi_mat_files)
+            ret['bmi_target_mat_file'].append(bmitarget_files[0] if len(bmitarget_files) == 1 else None)
+            ret['baseline_mat_file'].append(baseline_files[0] if len(baseline_files) == 1 else None)
+            ret['flag_baseline_online'].append(True if len(baseline_files) > 1 else False)
+            ret['flag_bmi_target'].append(True if len(bmitarget_files) > 1 else False)
+            ret['target_calibration_mat_file'].append(target_files[0] if len(target_files) == 1 else None)
+            ret['flag_target_calibration'].append(True if len(target_files) > 1 else False)
+            ret['holostim_seq_mat_file'].append(holostim_seq_files[0] if len(holostim_seq_files) == 1 else None)
             if not holostim_seq_files or not bmi_files or not baseline_files:  
-                ret['Flag_experiment_mat_file'].append(True) 
+                ret['flag_experiment_mat_file'].append(True) 
             else:
-                ret['Flag_experiment_mat_file'].append(False)
-            ret['Flag_Holostim_seq_mat_file'].append(True if len(holostim_seq_files) > 1 else False)
-            ret['MainProt_mat_file'].append(mainProt_files[0] if len(mainProt_files) == 1 else None)
-            ret['Flag_MainProt_mat_file'].append(True if len(mainProt_files) > 1 else False)
+                ret['flag_experiment_mat_file'].append(False)
+            ret['flag_holostim_seq_mat_file'].append(True if len(holostim_seq_files) > 1 else False)
+            ret['main_prot_mat_file'].append(mainProt_files[0] if len(mainProt_files) == 1 else None)
+            ret['flag_main_prot_mat_file'].append(True if len(mainProt_files) > 1 else False)
             
             if len(bmi_files) == 2:
                 bmi_files.sort(key=lambda x: x[1])
-                ret['Pretrain_mat_file'].append(bmi_files[0][0])
-                ret['BMI_mat_file'].append(bmi_files[1][0])
-                ret['Flag_BMI'].append(False)
+                ret['pretrain_mat_file'].append(bmi_files[0][0])
+                ret['bmi_mat_file'].append(bmi_files[1][0])
+                ret['flag_bmi'].append(False)
             else:
-                ret['Pretrain_mat_file'].append(None)
-                ret['BMI_mat_file'].append(None)
-                ret['Flag_BMI'].append(True if bmi_files else False)
+                ret['pretrain_mat_file'].append(None)
+                ret['bmi_mat_file'].append(None)
+                ret['flag_bmi'].append(True if bmi_files else False)
 
     # Normalize DataFrame column lengths
     df = pd.DataFrame.from_dict(ret, orient='index').transpose()
     df = df.loc[:, list(ret.keys())]  # Ensure column order matches append order
-    return df[list(df.columns[:6]) + sorted(df.columns[6:], key=str.casefold)].rename(columns=str.lower)
+    return df[list(df.columns[:6]) + sorted(df.columns[6:], key=str.casefold)]
 
 
 
-# cd df_hE2_rew = get_sessions_df('hE2_rew')
-# df_hE2_norew = get_sessions_df('hE2_norew')
+# cd df = get_sessions_df('hE2_rew')
 
 # df.to_parquet('df_holobmi.parquet', engine='pyarrow', index=False)
 
