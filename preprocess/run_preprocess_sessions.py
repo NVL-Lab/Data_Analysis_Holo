2f5aa39990a7f77c6edea0487081dc7470d97be3
Saul Gurgua Lopez <sgurgua4@uab.edu>
2026-01-07 13:11:24 -0600

    local preprocess check2

--- preprocess/run_preprocess_sessions.py
+++ preprocess/run_preprocess_sessions.py
@@ -11,7 +11,7 @@ from typing import Tuple, Optional
 #from preprocess.preprocess_suite2p import process_1_session_suite2p_offline
 
 
-def run_all_suite2p_local(df: pd.DataFrame, default_path: Path, folder_save: Path, folder_raw: Path, frame_rate:float):
+def run_all_suite2p_local(df_path: str, folder_save: str, folder_raw: str, default_path: Path, frame_rate:float):
     """ function to run and process all experiments with suite2p locally"""
 
     # df: /home/sgurgua4/Documents/project/nvl_lab/Data_Analysis_Holo/holobmi_df.parquet
@@ -20,16 +20,15 @@ def run_all_suite2p_local(df: pd.DataFrame, default_path: Path, folder_save: Pat
     # folder_raw: /data/project/nvl_lab/HoloBMI/Raw
     # frame_rate: 29.752
 
-    #folder_save = Path(folder_save)
-    df = pd.read_parquet(df)
-    for row in df.itertuples():
-        print(row)
-        folder_process = folder_save / 'process'
-        folder_raw_experiment = Path(folder_raw) / row['session_path']
-        folder_processed_experiment = Path(folder_process) / row['session_path']
+    folder_process = Path(folder_save).resolve(strict=True) / 'process'
+    folder_raw = Path(folder_raw).resolve(strict=True)
+
+    for row in pd.read_parquet(df_path).itertuples():
+        folder_raw_experiment = folder_raw / row['session_path']
+        folder_processed_experiment = folder_process / row['session_path']
         folder_suite2p = folder_processed_experiment / 'suite2p' / 'plane0'
-        if not Path(folder_suite2p).exists():
-            Path(folder_suite2p).mkdir(parents=True, exist_ok=True)
+        if not folder_suite2p.exists():
+            folder_suite2p.mkdir(parents=True, exist_ok=True)
         folder_im_paths = [str(folder_raw_experiment / 'im' / row['Holostim_seq_im']),
                            str(folder_raw_experiment / 'im' / row['Baseline_im']),
                            str(folder_raw_experiment / 'im' / row['Pretrain_im']),
