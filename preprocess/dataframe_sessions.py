7ba305e7a8092a5601a303c99a842cd9f3e34fd5
Saul Gurgua Lopez <sgurgua4@uab.edu>
2026-06-11 17:47:52 -0500

    edit df creation and session paths

--- preprocess/dataframe_sessions.py
+++ preprocess/dataframe_sessions.py
@@ -11,6 +11,7 @@ from datetime import datetime
 import re
 from preprocess.session_paths import _hE2_rew,_hE2_norew,_hE2_rew_fb,_hE3_rew,_No_Reward_Pretrain,_randrew,_randrew_fb,_BMI
 from utils.analysis_constants import AnalysisConstants as act
+from get_session_paths import get_session_paths
 
 # def get_all_sessions() -> pd.DataFrame:
 #     """ function to get a df with all sessions"""
@@ -285,31 +286,21 @@ def get_sessions_df(experiment_type: str) -> pd.DataFrame:
     df = df.loc[:, list(ret.keys())]  # Ensure column order matches append order
     return df[list(df.columns[:6]) + sorted(df.columns[6:], key=str.casefold)]
 
-def get_sessions(experiment_type: str) -> pd.DataFrame:
-    experiment_types = {
-        'hE2_rew': _hE2_rew,
-        'hE2_norew': _hE2_norew,
-        'hE3_rew': _hE3_rew,
-        'randrew': _randrew,
-        'randrew_fb': _randrew_fb,
-        'hE2_rew_fb': _hE2_rew_fb,
-        'No_Reward_Pretrain': _No_Reward_Pretrain,
-        'BMI': _BMI
-    }
-
-    if experiment_type is None:
-        return get_all_sessions()
-    elif experiment_type not in experiment_dict:
-        raise ValueError(f'Invalid experiment type: {experiment_type}. Choose from {", ".join(experiment_dict.keys())}')
-
-    ret = collections.defaultdict(list)
+def get_sessions(experiment_type: str = None) -> pd.DataFrame:
     raw_path = Path('/data/project/nvl_lab/HoloBMI/Raw')
+    matches = raw_path.glob("[0-9][0-9][0-9][0-9][0-9][0-9]/NVI*/D*")
+    session_paths = [p for p in matches if p.is_dir()]
 
     if experiment_type in act.experiment_types:
-        print(experiment_type)
-
-    matches = raw_path.glob("[0-9][0-9][0-9][0-9][0-9][0-9]/NVI*/D*")
-    sessions = [p for p in matches if p.is_dir()]
+        experiment_session_paths = get_session_paths()[experiment_type]
+        extra_session_paths = set(experiment_session_paths) - set(session_paths)
+        if len(extra_session_paths) > 0:
+            print(extra_session_paths)
+            exit()
+        session_paths = experiment_session_paths
+    else:
+        print('Doing all paths')
+        #raise ValueError(f'Invalid experiment type: {experiment_type} Choose from {", ".join(act.experiment_types)}')
 
     frame_count = {
         'baseline': act.calibration_frames,
@@ -318,8 +309,9 @@ def get_sessions(experiment_type: str) -> pd.DataFrame:
         'HoloVTA_pretrain': act.bmi_frames
     }
 
-    for session_path in sessions:
-        session_date, mouse_id, day_index = session_path.split('/')
+    sessions = []
+    for session_path in session_paths:
+        session_date, mouse_id, day_index = session_path.parts
 
         row = {
             'mouse_id': mouse_id,
@@ -332,10 +324,12 @@ def get_sessions(experiment_type: str) -> pd.DataFrame:
         }
 
         # imaging
-        parent_im_path = raw_path / session_path / 'im'
+        abs_session_path = raw_path / session_path
+        parent_im_path = abs_session_path / 'im'
 
         if not parent_im_path.exists():
-            print('wrong')
+            print(f'no im: {parent_im_path}')
+            exit()
 
         for experiment in frame_count:
             im_path = next(Path(parent_im_path / experiment).glob(f'{experiment}_{session_date}T*'), None)
@@ -344,237 +338,46 @@ def get_sessions(experiment_type: str) -> pd.DataFrame:
             if im_path:
                 tiff_count = sum(1 for f in im_path.iterdir() if f.is_file() and f.suffix.lower() == '.tif')
             else:
-                print('no imaging path')
+                print(f'no im: {im_path}')
+                exit()
 
             if tiff_count > frame_count[experiment]:
                 print('too many tiff')
             elif tiff_count == 0:
-                print('no tiff')
+                print(f'no tiff: {im_path}')
+                exit()
 
-            volt_path = im_path.glob(f'{im_path.name}_Cycle00001_VoltageRecording_001.csv')
+            volt_path = next(im_path.glob(f'{im_path.name}_Cycle00001_VoltageRecording_001.csv'), None)
 
-            if not volt_path:
-                print('no voltage recording')
-
-        # Additional file processing
-        #cont
-
-
-
-    for mouse_name, session in experiment_dict[experiment_type].items():
-        for day_index, session_path in enumerate(sessions):
-
-
-            session_date, mice_name, day_init = session_path.split('/')
-            ret['mice_name'].append(str(mice_name))
-            ret['session_date'].append(str(int(float(session_date))))
-            ret['day_init'].append(day_init)
-            ret['experiment_type'].append(str(experiment_type))
-            ret['session_path'].append(str(session_path))
-            ret['day_index'].append(str(int(float(day_index))))
-
-            dir_files = folder_raw / session_path
-            category_map = {
-                'baseline': 'baseline_im',
-                'BMI': 'bmi_im',
-                'holostim_seq': 'holostim_seq_im'  # Updated naming
-            }
-            tiff_limits = {
-                "holostim_seq": act.seq_holo_frames,
-                "baseline": act.calibration_frames,
-                "BMI": act.bmi_frames,
-                "pretrain": act.bmi_frames
-            }
-            # Initialize data columns with None, Flags with False
-            for category in category_map.values():
-                if f'flag_session_more_tif_{category}' not in ret:
-                    ret[f'flag_session_more_tif_{category}'] = []
-                ret[category].append(None)
-                ret[f'{category}_voltage_file'].append(None)  # Updated voltage naming
-                ret[f'flag_{category}'].append(False)  # Flagging for multiple files
-                ret[f'flag_session_more_tif_{category}'].append(False)
-                ret[f'flag_{category}_voltage_file'].append(False)
-
-            ret['pretrain_im'].append(None)
-            ret['flag_pretrain_im'].append(False)
-            ret['pretrain_im_voltage_file'].append(None)
-            ret['flag_session_more_tif_pretrain'].append(False)
-            ret[f'flag_pretrain_im_voltage_file'].append(False)
-            # Iterate over session directory
-            for file_name in os.listdir(dir_files):
-                file_path = dir_files / file_name
-
-                # Look for im folders
-                if file_name.startswith('im'):
-                    for im_subfolder in os.listdir(file_path):
-                        im_subfolder_path = file_path / im_subfolder
-                        im_subfolders = os.listdir(file_path)  # Get all subfolders first
-                        for key, category in category_map.items():
-
-                            if not any(key in subfolder for subfolder in im_subfolders):
-                                ret[f'flag_{category}'][-1] = True
-                            if im_subfolder.startswith(key):
-                                if not os.listdir(im_subfolder_path):
-                                    ret[f'flag_{category}'][-1] = True
-                                    ret[f'flag_session_more_tif_{category}'][-1] = True
-                                    continue
-                                im_files = [subfolder for subfolder in os.listdir(im_subfolder_path) if
-                                            subfolder.startswith(f"{key}_")]
-                                parsed_folder_name = f"{key}/{im_files[0]}" if im_files else None
-                                # Check if multiple files exist
-                                if len(im_files) > 1 or not im_files:
-                                    ret[f'flag_{category}'][-1] = True  # Flag session
-                                    ret[category][-1] = None  # No single file assigned
-                                    ret[f'flag_session_more_tif_{category}'][-1] = False
-
-                                elif len(im_files) == 1:
-                                    ret[category][-1] = parsed_folder_name
-                                    ret[f'flag_{category}'][-1] = False
-                                    parsed_folder = im_subfolder_path / im_files[0]
-                                    num_tiff_files = sum(
-                                        1 for file in os.listdir(parsed_folder) if file.endswith(".tif"))
-
-                                    ret[f'flag_session_more_tif_{category}'][-1] = True if num_tiff_files > tiff_limits[
-                                        key] else False
-                                else:
-                                    ret[category][-1] = None
-                                    ret[f'flag_{category}'][-1] = False  # No flag needed
-                                    ret[f'flag_session_more_tif_{category}'][-1] = False
-
-                                    # Look for voltage recording file inside the correct subfolder
-                                if im_files:
-                                    full_subfolder_name = im_files[0]
-                                    voltage_file_pattern = f"{full_subfolder_name}_Cycle00001_VoltageRecording_001.csv"
-                                    voltage_file_path = im_subfolder_path / full_subfolder_name / voltage_file_pattern
-
-                                    if voltage_file_path.exists():
-                                        ret[f'{category}_voltage_file'][-1] = voltage_file_pattern
-                                        ret[f'flag_{category}_voltage_file'][-1] = False
-                                    else:
-                                        ret[f'{category}_voltage_file'][-1] = None
-                                        ret[f'flag_{category}_voltage_file'][-1] = True
-
-                        # Checking if "_pretrain" is missing in any subfolder names
-                        if not any("_pretrain" in subfolder for subfolder in im_subfolders):
-                            ret['flag_pretrain_im'][-1] = True
-
-                        if "_pretrain" in im_subfolder:
-                            if not os.listdir(im_subfolder_path):
-                                ret['flag_pretrain_im'][-1] = True
-                                ret['flag_session_more_tif_pretrain'][-1] = True
-                                continue
-                            im_files = [subfolder for subfolder in os.listdir(im_subfolder_path) if
-                                        "_pretrain" in subfolder]
-                            parsed_folder_name = f"{im_subfolder}/{im_files[0]}" if im_files else None
-
-                            if len(im_files) > 1 or not im_files:
-                                ret['flag_pretrain_im'][-1] = True
-                                ret['pretrain_im'][-1] = None
-                                ret['flag_session_more_tif_pretrain'][-1] = False
-
-                            elif len(im_files) == 1:
-                                ret['pretrain_im'][-1] = parsed_folder_name
-                                ret['flag_pretrain_im'][-1] = False
-                                parsed_folder = im_subfolder_path / im_files[0]
-                                num_tiff_files = sum(1 for file in os.listdir(parsed_folder) if file.endswith(".tif"))
-                                if num_tiff_files > tiff_limits['pretrain']:
-                                    ret['flag_session_more_tif_pretrain'][-1] = True
-                                else:
-                                    ret['flag_session_more_tif_pretrain'][-1] = False
-
-                            else:
-                                ret['pretrain_im'][-1] = None
-                                ret['flag_pretrain_im'][-1] = False
-                                ret['flag_session_more_tif_pretrain'][-1] = False
-
-                            if im_files:
-                                full_subfolder_name = im_files[0]
-                                pretrain_voltage_file_pattern = f"{full_subfolder_name}_Cycle00001_VoltageRecording_001.csv"
-                                pretrain_voltage_file_path = im_subfolder_path / full_subfolder_name / pretrain_voltage_file_pattern
-
-                                if pretrain_voltage_file_path.exists():
-                                    ret['pretrain_im_voltage_file'][-1] = pretrain_voltage_file_pattern
-                                    ret[f'flag_pretrain_im_voltage_file'][-1] = False
-                                else:
-                                    ret['pretrain_im_voltage_file'][-1] = None
-                                    ret[f'flag_pretrain_im_voltage_file'][-1] = True
-
-            # Additional file processing
-            roi_mat_files = []
-            target_files = []
-            bmitarget_files = []
-            bmi_files = []
-            baseline_files = []
-            holostim_seq_files = []
-            mainProt_files = []
-
-            for file_name in os.listdir(dir_files):
-                if file_name.startswith('roi'):
-                    roi_mat_files.append(file_name)
-                elif file_name[:5] == 'BOT_c':
-                    ret['bot_candidates'].append(str(file_name))
-                elif file_name[:5] == 'BOT_e':
-                    ret['bot_ensemble'].append(str(file_name))
-                elif file_name[:5] == 'GPL_c':
-                    ret['gpl_candidates'].append(str(file_name))
-                elif file_name[:5] == 'GPL_e':
-                    ret['gpl_ensemble'].append(str(file_name))
-                elif file_name[:5] == 'XML_c':
-                    ret['xml_candidates'].append(str(file_name))
-                elif file_name[:5] == 'XML_e':
-                    ret['xml_ensemble'].append(str(file_name))
-                elif file_name.startswith('holostim_seq') and file_name.endswith('.mat'):
-                    # ret['Holostim_seq_mat_file'].append(file_name)
-                    holostim_seq_files.append(str(file_name))
-                elif file_name.startswith('workspace'):
-                    ret['workspace_mat_file'].append(str(file_name))
-                elif file_name.startswith('holoMask'):
-                    ret['holomask_gpl_file'].append(str(file_name))
-                elif file_name.startswith('mainProt'):
-                    # ret['MainProt_file'].append(str(file_name))
-                    mainProt_files.append(str(file_name))
-                elif file_name.startswith('seq_single'):
-                    ret['xml_holostim_seq'].append(str(file_name))
-                elif file_name.startswith('strcMask'):
-                    ret['strc_mask_mat_file'].append(str(file_name))
-                elif file_name.startswith('BMI_target_info'):
-                    bmitarget_files.append(str(file_name))
-                elif file_name.startswith('BaselineOnline') and file_name.endswith('.mat'):
-                    baseline_files.append(str(file_name))
-                elif file_name.startswith('target_calibration'):
-                    target_files.append(str(file_name))
-                elif file_name.startswith('BMI_online'):
-                    match = re.search(r'BMI_online(\d{6}T\d{6})', file_name)
-                    if match:
-                        bmi_files.append((file_name, datetime.strptime(match.group(1), "%y%m%dT%H%M%S")))
-
-            ret['roi_mat_file'].extend(roi_mat_files)
-            ret['bmi_target_mat_file'].append(bmitarget_files[0] if len(bmitarget_files) == 1 else None)
-            ret['baseline_mat_file'].append(baseline_files[0] if len(baseline_files) == 1 else None)
-            ret['flag_baseline_online'].append(True if len(baseline_files) > 1 else False)
-            ret['flag_bmi_target'].append(True if len(bmitarget_files) > 1 else False)
-            ret['target_calibration_mat_file'].append(target_files[0] if len(target_files) == 1 else None)
-            ret['flag_target_calibration'].append(True if len(target_files) > 1 else False)
-            ret['holostim_seq_mat_file'].append(holostim_seq_files[0] if len(holostim_seq_files) == 1 else None)
-            if not holostim_seq_files or not bmi_files or not baseline_files:
-                ret['flag_experiment_mat_file'].append(True)
+            if volt_path:
+                row[f'{experiment.lower()}_voltage_file'] = volt_path
             else:
-                ret['flag_experiment_mat_file'].append(False)
-            ret['flag_holostim_seq_mat_file'].append(True if len(holostim_seq_files) > 1 else False)
-            ret['main_prot_mat_file'].append(mainProt_files[0] if len(mainProt_files) == 1 else None)
-            ret['flag_main_prot_mat_file'].append(True if len(mainProt_files) > 1 else False)
+                print(f'no voltage recording: {volt_path}')
 
-            if len(bmi_files) == 2:
-                bmi_files.sort(key=lambda x: x[1])
-                ret['pretrain_mat_file'].append(bmi_files[0][0])
-                ret['bmi_mat_file'].append(bmi_files[1][0])
-                ret['flag_bmi'].append(False)
-            else:
-                ret['pretrain_mat_file'].append(None)
-                ret['bmi_mat_file'].append(None)
-                ret['flag_bmi'].append(True if bmi_files else False)
+        # Check for flags
+        row['roi_mat_file'] = next(abs_session_path.glob('roi*'), None)
+        row['bot_candidates'] = next(abs_session_path.glob('BOT_c*'), None)
+        row['bot_ensemble'] = next(abs_session_path.glob('BOT_e*'), None)
+        row['gpl_candidates'] = next(abs_session_path.glob('GPL_c*'), None)
+        row['gpl_ensemble'] = next(abs_session_path.glob('GPL_e*'), None)
+        row['xml_candidates'] = next(abs_session_path.glob('XML_c*'), None)
+        row['xml_ensemble'] = next(abs_session_path.glob('XML_e*'), None)
+        row['holostim_seq_mat_file'] = next(abs_session_path.glob('holostim_seq*.mat'), None)
+        row['workspace_mat_file'] = next(abs_session_path.glob('workspace*'), None)
+        row['holomask_gpl_file'] = next(abs_session_path.glob('holoMask*'), None)
+        row['main_prot_mat_file'] = next(abs_session_path.glob('mainProt*'), None)
+        row['xml_holostim_seq'] = next(abs_session_path.glob('seq_single*'), None)
+        row['strc_mask_mat_file'] = next(abs_session_path.glob('strcMask*'), None)
+        row['bmi_target_mat_file'] = next(abs_session_path.glob('BMI_target_info*'), None)
+        row['baseline_mat_file'] = next(abs_session_path.glob('BaselineOnline*.mat'), None)
+        row['target_calibration_mat_file'] = next(abs_session_path.glob('target_calibration*'), None)
+        bmi_files = sorted((abs_session_path.glob('BMI_online*T*.mat')))
+        if len(bmi_files) == 2:
+            row['pretrain_mat_file'] = bmi_files[0]
+            row['bmi_mat_file'] = bmi_files[1]
+        else:
+            print('no bmi')
 
-    # Normalize DataFrame column lengths
-    df = pd.DataFrame.from_dict(ret, orient='index').transpose()
-    df = df.loc[:, list(ret.keys())]  # Ensure column order matches append order
-    return df[list(df.columns[:6]) + sorted(df.columns[6:], key=str.casefold)]
+    df_sessions = pd.DataFrame(sessions)
+
+    return df_sessions
